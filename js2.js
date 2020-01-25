346d5f16e533031610d7b98638a8a8d696722b09
michalwitek1232 <michalwitek1232@gmail.com>
2020-01-25 13:59:00 +0100

    tomaszewska

---
+++ js2.js
@@ -0,0 +1,115 @@
+var autoTest = "Dziwne słowo beelebee";
+var breakTest = "Dziwne słowdasdasdaso beelebee";
+
+
+
+function testxd() {
+    var zmienna = document.getElementById('answer-test').value;
+    var res = zmienna.toLowerCase();
+    switch (res) {
+    case "auto":
+        break;
+    case "break":
+        break;
+    case "case":
+        break;
+    case "char":
+         break;
+    case "const":
+        break;
+    case "continue":
+        break;
+    case "default":
+        break;
+    case "do":
+       break;
+    case "double":
+        break;
+    case "else":
+        break;
+    case "enum":
+        break;
+    case "extern":
+        break;
+    case "float":
+         break;
+    case "if":
+        break;
+    case "int":
+        break;
+    case "long":
+        break;
+    case "register":
+       break;
+    case "return":
+        break;    
+     case "short":
+        break;
+    case "signed":
+        break;
+    case "sizeof":
+       break;
+    case "static":
+        break;
+    case "struct":
+        break;
+    case "switch":
+        break;
+    case "typedef":
+        break;
+    case "union":
+         break;
+    case "unsigned":
+        break;
+    case "void":
+        break;
+    case "volatile":
+        break;
+    case "while":
+       break;
+    case "+":
+        break;   
+    case "=":
+        break;       
+    case "-":
+        break; 
+    case "/":
+        break;       
+    case "//":
+        break;     
+    case "*":
+        break;       
+    case "%":
+        break; 
+    case "=":
+        break;       
+    case "==":
+        break;
+    case ">":
+        break;       
+    case ">=":
+        break; 
+    case "<=":
+        break;       
+    case "<":
+        break;     
+    case "!=":
+        break;       
+    case "||":
+        break; 
+    case "&&":
+        break;       
+    case "!":
+        break;                                          
+    case "&":
+        break;   
+    case "^":
+        break;                    
+    default:
+        document.getElementById('output-test').innerHTML = "Sprawdź poprawność znaków i spróbuj ponownie";
+        break;
+}
+
+
+}
+
